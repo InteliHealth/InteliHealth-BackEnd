ca711cae3f226a0fa6f2c0fbbda4b6575c29f838
Kaue Camargo Marinho <48038990889@info.scs>
2022-03-24 17:19:14 -0300

    API iniciada

---
+++ API/InteliHealth/InteliHealth/Data/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InteliHealth.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ API/InteliHealth/InteliHealth/Domains/Topico.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InteliHealth.Domains
+{
+    public class Topico
+    {
+        [Key]
+        public int IdTopico { get; set; }
+
+        [ForeignKey("Usuario")]
+        public int IdUsuario { get; set; }
+        public Usuario Usuarios { get; set; }
+        public string Nome { get; set; }
+        public string Icone { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public ICollection<Topico> Topicos { get; set; }
+    }
+}
